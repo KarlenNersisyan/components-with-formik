e0e1a9a9e4ee37bb6bf4f78f187191ade68cc0c0
Karlen Nersisyan <karlennersisyan814@gmail.com>
2023-10-05 18:20:47 +0400

    Added FormSelect Component

---
+++ src/components/global/form/FormSelect/index.js
@@ -0,0 +1,39 @@
+//* Components
+import { Field, ErrorMessage } from 'formik';
+import TextError from './TextError';
+
+//* Styles
+import FormSelectStyle from './style';
+
+const FormSelect = (props) => {
+	const { label, name, options, ...rest } = props;
+
+	return (
+		<FormSelectStyle className='form-control'>
+			<label htmlFor={name}>{label}</label>
+
+			<Field
+				id={name}
+				name={name}
+				as='select'
+				{...rest}>
+				{options.map((option) => {
+					return (
+						<option
+							key={option.value}
+							value={option.value}>
+							{option.key}
+						</option>
+					);
+				})}
+			</Field>
+
+			<ErrorMessage
+				name={name}
+				component={TextError}
+			/>
+		</FormSelectStyle>
+	);
+};
+
+export default FormSelect;
---
+++ src/components/global/form/FormSelect/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const FormSelectStyle = styled.div`
+	&.form-control {
+	}
+`;
+
+export default FormSelectStyle;
