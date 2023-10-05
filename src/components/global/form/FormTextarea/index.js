6995cd4faa4d08f66144aa58f7cf9ad1e1ee2f6b
Karlen Nersisyan <karlennersisyan814@gmail.com>
2023-10-05 18:21:06 +0400

    Added FormTextarea Component

---
+++ src/components/global/form/FormTextarea/index.js
@@ -0,0 +1,30 @@
+//* Components
+import { Field, ErrorMessage } from 'formik';
+import TextError from './TextError';
+
+//* Styles
+import FormTextareaStyle from './style';
+
+const FormTextarea = (props) => {
+	const { label, name, ...rest } = props;
+
+	return (
+		<FormTextareaStyle className='form-control'>
+			<label htmlFor={name}>{label}</label>
+
+			<Field
+				id={name}
+				name={name}
+				as='textarea'
+				{...rest}
+			/>
+
+			<ErrorMessage
+				name={name}
+				component={TextError}
+			/>
+		</FormTextareaStyle>
+	);
+};
+
+export default FormTextarea;
---
+++ src/components/global/form/FormTextarea/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const FormTextareaStyle = styled.div`
+	&.form-control {
+	}
+`;
+
+export default FormTextareaStyle;
